fa0a39dc9a108266e1306d20daa3a37b90c8d962
meowmeowmac <saisann.lone0111@gmail.com>
2025-06-22 02:44:25 +0800

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome retard faggot bitch</ThemedText>
+        <ThemedText type="title">Welcome nigger</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
