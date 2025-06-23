60237836151b6fea0b20a969145eb9cbb38e2609
sambalbalado <mohdesaam123@gmail.com>
2025-06-23 17:00:59 +0800

    Update on home page title
    Changed it into an APPROPRIATE text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Sai sann i wanna suck your dick tonight</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
